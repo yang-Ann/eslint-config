9c31a3f71a3a9e9f90276e5fec8cd89fe0320fac
An <331536495@qq.com>
2023-05-08 22:51:51 +0800

    修改 eslint-config-ts 配置

--- packages/eslint-config-ts/index.js
+++ packages/eslint-config-ts/index.js
@@ -23,7 +23,7 @@ module.exports = {
   rules: {
     "no-var": "error", // 禁止 var
     "default-case": "error", // 在switch语句中需要有default语句
-    "no-async-promise-executor": "off", // async 处理器函数可以是异步的
+    "no-async-promise-executor": "warn", // async 处理器函数可以是异步的
     "@typescript-eslint/no-inferrable-types": "off", // 可以声明接口类型为 any
     "@typescript-eslint/no-explicit-any": "off", // 可以声明类型为 any
     "@typescript-eslint/ban-ts-comment": "off", // 允许特殊的注释, @ts-xxx
